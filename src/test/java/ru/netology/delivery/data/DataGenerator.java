8128661770c34cdf2116970c5ff216eccceb2dad
Emma Paris <eparis@atlassian.com>
2023-06-09 13:57:55 +0300

    DataGenerator1

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -14,29 +14,25 @@ public class DataGenerator {
 
     public static String generateDate(int shift) {
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
+
     }
 
     public static String generateCity() {
         var cities = new String[]{"Уфа", "Махачкала", "Черкесск", "Саранск", "Казань", "Чита", "Краснодар", "Пермь", "Владивосток",
                 "Хабаровск", "Брянск", "Владимир", "Воронеж", "Самара"};
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
+
         return cities[new Random().nextInt(cities.length)];
 
     }
 
     public static String generateName(String locale) {
         var faker = new Faker(new Locale(locale));
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
+
         return faker.name().lastName() + " " + faker.name().firstName();
     }
 
     public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
+
         var faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
@@ -46,8 +42,7 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
+
             return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
     }
